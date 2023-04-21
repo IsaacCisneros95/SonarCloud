86dedc109811777445330674bd6d73365ca0fe94
IsaacCisneros95 <jose.isaac.cisneros95@gmail.com>
2023-04-21 14:31:35 -0700

    added test and changed config

--- test/sonar_cloud.test.ts
+++ test/sonar_cloud.test.ts
@@ -1,17 +1,14 @@
-// import * as cdk from 'aws-cdk-lib';
+import * as cdk from "aws-cdk-lib";
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as SonarCloud from '../lib/sonar_cloud-stack';
+import { SonarCloudStack } from "../lib/sonar_cloud-stack";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/sonar_cloud-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new SonarCloud.SonarCloudStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe("when instantiating SonarCloudStack", () => {
+  it("doesnt throw any errors", () => {
+    const app = new cdk.App();
+
+    expect(() => new SonarCloudStack(app, "SonarCloudStack")).not.toThrow();
+  });
 });
